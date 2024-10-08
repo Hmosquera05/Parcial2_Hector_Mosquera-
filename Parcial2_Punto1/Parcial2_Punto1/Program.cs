c19407c3e76dfc17158b492225e93e164d4653ee
Héctor Alfredo Mosquera Coche <mosquerah59@gmail.com>
2024-10-07 21:04:29 -0500

    Declaramos el # de participantes entre 2 y 4

--- Parcial2_Punto1/Parcial2_Punto1/Program.cs
+++ Parcial2_Punto1/Parcial2_Punto1/Program.cs
@@ -8,4 +8,12 @@
         {
             jugarOtraVez = false;
             Console.Clear();
-            Console.WriteLine("Hola! Vas a jugar el juego 'Adivina el número'!");
+            Console.WriteLine("Hola! Vas a jugar al juego 'Adivina el número'. El juego es muy sencillo, consiste en que los participantes comenzarán en orden a jugar y se turnarán para ingresar por pantalla un número ℕ hasta acertar el número oculto y salir ganador.!");
+
+            int numJugadores;
+            do
+            {
+                Console.Write("Ingrese el número de jugadores (2-4): ");
+            } while (!int.TryParse(Console.ReadLine(), out numJugadores) || numJugadores < 2 || numJugadores > 4);
+
+           
