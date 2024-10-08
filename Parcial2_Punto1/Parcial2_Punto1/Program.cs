4b5f0b52abda0178386cc6398ce85d08254c0419
Héctor Alfredo Mosquera Coche <mosquerah59@gmail.com>
2024-10-07 20:54:16 -0500

    Se inicia el programa y se establece un bucle que permite jugar varias veces

---
+++ Parcial2_Punto1/Parcial2_Punto1/Program.cs
@@ -0,0 +1,11 @@
+﻿public class Program
+{
+    static void Main()
+    {
+        bool jugarOtraVez;
+
+        do
+        {
+            jugarOtraVez = false;
+            Console.Clear();
+            Console.WriteLine("Hola! Vas a jugar el juego 'Adivina el número'!");
