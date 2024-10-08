21f37a8a9bb619a40be206feac56951b5da8e69f
Héctor Alfredo Mosquera Coche <mosquerah59@gmail.com>
2024-10-07 22:31:40 -0500

    Entrada de datos: Se solicita la cantidad de estudiantes y sus notas. Cada nota se almacena en una lista.

---
+++ Parcial2_Punto2/Parcial2_Punto2/Program.cs
@@ -0,0 +1,21 @@
+﻿public class Program
+{
+    static void Main(string[] args)
+    {
+        do
+        {
+            // Lista para almacenar las notas de los estudiantes
+            List<double> notasEstudiantes = new List<double>();
+            int estudiantesGanaron = 0;
+            int estudiantesPerdieron = 0;
+            string continuar;
+
+            // Ingresar notas de los estudiantes
+            Console.WriteLine("Ingrese la cantidad de estudiantes:");
+            int cantidadEstudiantes = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < cantidadEstudiantes; i++)
+            {
+                Console.WriteLine($"Ingrese la nota del estudiante {i + 1}:");
+                double nota = double.Parse(Console.ReadLine());
+                notasEstudiantes.Add(nota);
